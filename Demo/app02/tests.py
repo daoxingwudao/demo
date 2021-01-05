c5d5011a428bf188c32d4627c8133e9167b8069d
卢逢卫 <daoxingwudao@163.com>
2021-01-05 11:16:44 +0800

    分支修改

--- Demo/app02/tests.py
+++ Demo/app02/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Demo.settings")
+    import django
